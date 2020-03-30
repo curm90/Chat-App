32628a2c1ba66eddb25de7b2f55cfea63da16fa3
Liam Sutton <devliam90@gmail.com>
2020-03-30 07:41:30 +0100

    challenge: send welcome message to new users

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,5 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-  console.log('Count has been updated', count);
-});
-
-document.querySelector('#increment').addEventListener('click', () => {
-  console.log('clicked');
-  socket.emit('increment');
+socket.on('message', (message) => {
+  console.log(message);
 });
--- src/index.js
+++ src/index.js
@@ -12,19 +12,10 @@ const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-let count = 0;
-
 io.on('connection', (socket) => {
   console.log('new web socket connection');
 
-  socket.emit('countUpdated', count);
-
-  socket.on('increment', () => {
-    count++;
-
-    // socket.emit('countUpdated', count);
-    io.emit('countUpdated', count);
-  });
+  socket.emit('message', 'Welcome User!');
 });
 
 server.listen(port, () => {
