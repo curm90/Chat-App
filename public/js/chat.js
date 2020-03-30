876a14fd16b98d4a02916944a51af84ceca4d464
Liam Sutton <devliam90@gmail.com>
2020-03-30 12:52:06 +0100

    challenge: get timestamp rendering for location

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,16 +18,20 @@ socket.on('message', (message) => {
   console.log(message);
 
   const html = Mustache.render(messageTemplate, {
-    message
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a')
   });
 
   $messages.insertAdjacentHTML('beforeend', html);
 });
 
-socket.on('locationMessage', (url) => {
-  console.log(url);
+socket.on('locationMessage', (message) => {
+  console.log(message);
 
-  const html = Mustache.render(locationMessageTemplate, { url });
+  const html = Mustache.render(locationMessageTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a')
+  });
 
   $messages.insertAdjacentHTML('beforeend', html);
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
+const {
+  generateMessage,
+  generateLocationMessage
+} = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,8 +20,11 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new web socket connection');
 
-  socket.emit('message', 'Welcome User!');
-  socket.broadcast.emit('message', 'A new user has joined');
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit(
+    'message',
+    generateMessage('A new user has joined')
+  );
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter();
@@ -26,20 +33,22 @@ io.on('connection', (socket) => {
       return callback('Profanity is not allowed');
     }
 
-    io.emit('message', message);
+    io.emit('message', generateMessage(message));
     callback('Delivered!');
   });
 
   socket.on('sendLocation', (location, callback) => {
     io.emit(
       'locationMessage',
-      `https://google.com/maps?=${location.latitude},${location.longitude}`
+      generateLocationMessage(
+        `https://google.com/maps?=${location.latitude},${location.longitude}`
+      )
     );
     callback();
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left');
+    io.emit('message', generateMessage('A new user has joined'));
   });
 });
 
