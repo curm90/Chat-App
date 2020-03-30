e33b66e446cfc85581434a08eb5bcf7a97d722b7
Liam Sutton <devliam90@gmail.com>
2020-03-30 07:35:01 +0100

    count example

--- src/index.js
+++ src/index.js
@@ -12,8 +12,19 @@ const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-io.on('connection', () => {
+let count = 0;
+
+io.on('connection', (socket) => {
   console.log('new web socket connection');
+
+  socket.emit('countUpdated', count);
+
+  socket.on('increment', () => {
+    count++;
+
+    // socket.emit('countUpdated', count);
+    io.emit('countUpdated', count);
+  });
 });
 
 server.listen(port, () => {
