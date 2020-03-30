5953258a1cc5f9068112fb97edc04e3f0c923b4b
Liam Sutton <devliam90@gmail.com>
2020-03-30 08:05:54 +0100

    tweak input target

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,10 +4,12 @@ socket.on('message', (message) => {
   console.log(message);
 });
 
-document.querySelector('form').addEventListener('submit', (e) => {
-  e.preventDefault();
+document
+  .querySelector('#message-form')
+  .addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  const message = document.querySelector('input').value;
+    const message = e.target.elements.message.value;
 
-  socket.emit('sendMessage', message);
-});
+    socket.emit('sendMessage', message);
+  });
