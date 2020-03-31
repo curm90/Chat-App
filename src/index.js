f38b7313e8af6f3e276e9a3a9c5f6664ccec932c
Liam Sutton <devliam90@gmail.com>
2020-03-31 16:58:56 +0100

    add error for same user in room

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const {
   generateMessage,
   generateLocationMessage
 } = require('./utils/messages');
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require('./utils/users.js');
 
 const app = express();
 const server = http.createServer(app);
@@ -20,13 +26,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new web socket connection');
 
-  socket.on('join', ({ username, room }) => {
-    socket.join(room);
+  socket.on('join', ({ username, room }, callback) => {
+    const { error, user } = addUser({
+      id: socket.id,
+      username,
+      room
+    });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
 
     socket.emit('message', generateMessage('Welcome!'));
     socket.broadcast
-      .to(room)
-      .emit('message', generateMessage(`${username} has joined!`));
+      .to(user.room)
+      .emit(
+        'message',
+        generateMessage(`${user.username} has joined!`)
+      );
+
+    callback();
   });
 
   socket.on('sendMessage', (message, callback) => {
@@ -51,7 +72,14 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', generateMessage('A new user has joined'));
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        generateMessage(`${user.username} has left`)
+      );
+    }
   });
 });
 
